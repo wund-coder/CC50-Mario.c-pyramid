d9054400dda54c704550cdfec8490b13e0954627
wund-coder <wundantunes@gmail.com>
2026-01-13 19:48:57 -0300

    mario.c

--- mario.c
+++ mario.c
@@ -1,30 +1,21 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 int main()
 {
-	int n;
-
+	int n; //Variável agregado ao "n".
 	do
 	{
-      printf("Altura: ");
+      printf("Altura: "); //Altura solicitada e imprimida ao usuário, logo em seguida escaneada.
       scanf("%d", &n);
 	}
 	
-		while (n<1 || n>8);
+		while (n<1 || n>8);//Limitador da altura enviada.
 		
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++) //Laço que repetirá as hashtags verticalmente conforme o número enviado demanda.
 {
-		for (int j = 0; j <= i; j++)
+		for (int j = 0; j <= i; j++) //Laço que repetirá as hashtags horizontalmente conforme o número enviado demanda.
     {
-    printf("#");
+    printf("#"); //Imprimirá na tela do usuário as hashtags.
     }
     printf("\n");
 }
